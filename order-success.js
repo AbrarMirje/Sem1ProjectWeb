1d2975a8c332d47cf6fa2882ffe372a7dbe7fa39
AbrarMirje <abrarmirje5@gmail.com>
2023-12-22 08:36:37 -0800

    alert changed

--- order-success.js
+++ order-success.js
@@ -1,9 +1,11 @@
 document.querySelectorAll('.order-now').forEach(function (element) {
     element.addEventListener('click', function () {
         Swal.fire({
+            position: "centre",
+            icon: "success",
             title: "Order Successful!",
-            text: "Enjoy the Meal!",
-            icon: "success"
-        });
+            showConfirmButton: false,
+            timer: 1500
+          });
     });
 });
